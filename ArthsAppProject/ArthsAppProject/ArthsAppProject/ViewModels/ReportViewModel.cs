7cb847f59b93f103b6270826f25cad71bcdd32e2
viperee <just.b.nd.l@gmail.com>
2018-06-13 01:34:01 +0200

    Test microcharts

--- ArthsAppProject/ArthsAppProject/ArthsAppProject/ViewModels/ReportViewModel.cs
+++ ArthsAppProject/ArthsAppProject/ArthsAppProject/ViewModels/ReportViewModel.cs
@@ -4,15 +4,50 @@ using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
 using ArthsAppProject.Infrastructure;
+using Microcharts;
+using SkiaSharp;
+using Microcharts.Forms;
 
 namespace ArthsAppProject.ViewModels
 {
     public class ReportViewModel : AppMapViewModelBase
     {
-
-
-        public ReportViewModel(INavigationService navigationService) : base (navigationService)
+        private Chart chart;
+        public Chart Chart
         {
+            get { return chart; }
+            set
+            {
+                chart = value;
+                RaisePropertyChanged(() => Chart);
+            }
+        }
+
+        public ReportViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            var entries = new[]
+            {
+                new Entry(200)
+                {
+                    Label = "January",
+                    ValueLabel = "200",
+                Color = SKColor.Parse("#266489")
+                },
+                new Entry(400)
+                {
+                Label = "February",
+                ValueLabel = "400",
+                Color = SKColor.Parse("#68B9C0")
+                },
+                new Entry(-100)
+                {
+                Label = "March",
+                ValueLabel = "-100",
+                Color = SKColor.Parse("#90D585")
+                }
+            };
+
+            Chart = new BarChart() { Entries = entries };
         }
     }
 }
