b05a6fecbd3357a9c7767003d79bf5f7be22ca23
aya <Aya.maoui@gmail.com>
2018-06-15 02:24:33 +0100

    [Aya] - Add Pain Management - Part01

---
+++ ArthsAppProject/ArthsAppProject/ArthsAppProject/Models/TypePainEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArthsAppProject.Models
+{
+    public enum TypePainEnum
+    {
+        Faible,
+        Moyen,
+        Elevé
+    };
+}
--- ArthsAppProject/ArthsAppProject/ArthsAppProject/ViewModels/PainEvaluationViewModel.cs
+++ ArthsAppProject/ArthsAppProject/ArthsAppProject/ViewModels/PainEvaluationViewModel.cs
@@ -4,15 +4,82 @@ using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
 using ArthsAppProject.Infrastructure;
+using System.ComponentModel;
+using System.Windows.Input;
+using Prism.Services;
+using System.Collections.Generic;
+using System.Linq;
+using ArthsAppProject.ViewModels.Base;
+using ArthsAppProject.ValidationRule;
+using Xamarin.Forms;
+using ArthsAppProject.Models;
 
 namespace ArthsAppProject.ViewModels
 {
     public class PainEvaluationViewModel : AppMapViewModelBase
     {
+        public ICommand SubmitCommand => new Command(() => OnSubmit());
 
+        public event PropertyChangedEventHandler PropertyChanged;
+        private IPageDialogService _dialogService;
+        private INavigationService _navigationService;
+        private TypePainEnum levelPainSelected;
 
-        public PainEvaluationViewModel(INavigationService navigationService) : base (navigationService)
+        public PainEvaluationViewModel(INavigationService navigationService, IPageDialogService dialogService) : base (navigationService)
         {
+            _dialogService = dialogService;
+            _navigationService = navigationService;
+        }
+
+        public TypePainEnum LevelPainSelected
+        {
+            get { return levelPainSelected; }
+            set
+            {
+                levelPainSelected = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("LevelPainSelected"));
+            }
+        }
+
+        public List<string> LevelPainName
+        {
+            get
+            {
+                return Enum.GetNames(typeof(TypePainEnum)).Select(b => b.SplitCamelCase()).ToList();
+            }
+        }
+
+        private DateTime datePain;
+        public DateTime DatePain
+        {
+            get
+            {
+                return datePain;
+            }
+            set
+            {
+                datePain = value;
+                RaisePropertyChanged(() => DatePain);
+            }
+        }
+        private string textCirconstance;
+        public string TextCirconstance
+        {
+            get
+            {
+                return textCirconstance;
+            }
+            set
+            {
+                textCirconstance = value;
+                RaisePropertyChanged(() => TextCirconstance);
+            }
+        }
+
+        private void OnSubmit()
+        {
+               _navigationService.NavigateAsync("PainEvolution");
+
         }
     }
 }
