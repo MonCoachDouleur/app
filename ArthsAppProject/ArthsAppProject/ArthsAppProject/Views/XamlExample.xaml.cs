40aaf8e08aa6e5a30db53202f6b182541c51b96c
Amaou <aya.maoui@gmail.com>
2018-06-22 21:17:32 +0200

    #25 Update FAQ

--- ArthsAppProject/ArthsAppProject/ArthsAppProject/Views/XamlExample.xaml.cs
+++ ArthsAppProject/ArthsAppProject/ArthsAppProject/Views/XamlExample.xaml.cs
@@ -97,23 +97,23 @@ namespace ArthsAppProject.Views
 
             var vFirstAccord = new AccordionSource()
             {
-                HeaderText = "First",
-                HeaderTextColor = Color.Black,
-                HeaderBackGroundColor = Color.Yellow,
+                HeaderText = "Qui sommes nous ?",
+                HeaderTextColor = Color.White,
+                HeaderBackGroundColor = Color.FromHex("#5EA6C2"),
                 ContentItems = vListViewTwo
             };
             vResult.Add(vFirstAccord);
             var vSecond = new AccordionSource()
             {
-                HeaderText = "Second ",
+                HeaderText = "Nous contacter",
                 HeaderTextColor = Color.White,
-                HeaderBackGroundColor = Color.FromHex("#77d065"),
+                HeaderBackGroundColor = Color.FromHex("#5EA6C2"),
                 ContentItems = vViewLayout
             };
             vResult.Add(vSecond);
             var vThird = new AccordionSource()
             {
-                HeaderText = "Third",
+                HeaderText = "Mentions légales",
                 HeaderTextColor = Color.White,
                 HeaderBackGroundColor = Color.Purple,
                 ContentItems = vListViewOne
